3cd59ed8745fe8811b3ee246c732ea69d0a61b54
Ton Martín <tonmg2006@gmail.com>
2026-07-16 21:55:00 +0200

    Main program hardcoded. No errors.

--- src/main.c
+++ src/main.c
@@ -9,12 +9,21 @@ int main() {
     vm_init(&vm);
 
     // Push 20, Push 5, 5 + 20 = 25, Store 25 in Register 1, Stop VM
-    uint8_t program[] = {
-        PSH, 0x14, 0x00, 0x00, 0x00, // Opcode, 20
-        PSH, 0x05, 0x00, 0x00, 0x00, // Opcode, 5
-        ADD, // Opcode (5 + 20)
-        SET, 0x01, 99, 0x63, 0x00, 0x00, // Opcode, Register 1, 99
-        HLT
+uint8_t program[] = {
+        SET, 0, 3, 0, 0, 0,    // R0 = 3
+        
+        // LOOP
+        GET, 0,                 // R0 -> Stack [3]
+        BZ, 28, 0, 0, 0,        // If Top == 0 jump to END
+        
+        GET, 0,                 // R0 -> Stack [3, 3]
+        PSH, 1, 0, 0, 0,        // Push 1 to Stack [1, 3, 3]
+        SUB,                    // 3 - 1 = 2 to Stack [2, 3]
+        STR, 0,                 // R0 = 2
+        JMP, 6, 0, 0, 0,        //Jump to LOOP
+        
+        // END
+        HLT                     // Stop VM
     };
 
     // Load program with index 0
